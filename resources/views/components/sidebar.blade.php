6a78c1f51804bb14f329f67f2a962fce764be425
Relchzen <104548363+Relchzen@users.noreply.github.com>
2023-10-23 09:33:55 +0700

    admin panel

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="bg-maroon w-64 pt-2 text-light font-comfortaa">
+    <a href="/" class="flex-none">
+        <div class="flex p-1 hover:cursor-pointer justify-center">
+            <x-lucide-cat class="w-7 h-7 text-light mb-1" />
+                <p class="text-light text-2xl font-comfortaa font-semibold">
+                    Nyanban
+                </p>
+        </div>
+    </a>
+    <nav>
+            <a href="/admin">Order</a>
+            <a href="/admin/menu">Menu</a>
+            <a href="/admin/category">Menu</a>
+            <a href="/admin/user">Menu</a>
+
+        </nav>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.layout')
 
 @section('content')
-        <div class="container px-28 bg-cream mx-auto">
+        <div class="container px-8 md:px-28 bg-cream mx-auto">
             Hello World
         </div>
 @endsection
